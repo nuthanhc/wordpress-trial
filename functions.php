51a6ead22a04bfa8220e5d61474d9fc6294e5c22
nuthanhc <nuthanhc07@gmail.com>
2022-05-25 00:08:45 +0530

    final push

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 function followandrew_theme_support(){
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme','followandrew_theme_support');
 
@@ -31,5 +32,42 @@ function followandrew_register_scripts(){
     wp_enqueue_script('followandrew-main', get_template_directory_uri(),"/assets/js/main.js",  array(),'1.0',true);
 }
 add_action('wp_enqueue_scripts','followandrew_register_scripts');
+
+?>
+<?php
+function followandrew_widget_areas(){
+    register_sidebar(
+        array(
+            'before_title'=> '',
+            'after_title'=> '',
+            'before_widget'=>'<ul class="social-list list-inline py-3 mx-auto">',
+            'after_widget'=>'</ul>',
+            'name'=> 'sidebar Area',
+            'id'=> 'sidebar-1',
+            'description' => 'sidebar Widget Area'
+            
+        )
+
+        );
+         
+        register_sidebar(
+            array(
+                'before_title'=> '',
+                'after_title'=> '',
+                'before_widget'=>'<ul class="social-list list-inline py-3 mx-auto">',
+                'after_widget'=>'</ul>',
+                'name'=> 'footer Area',
+                'id'=> 'footer-1',
+                'description' => 'footer Widget Area'
+           
+            )
+    
+            );
+        
+}
+
+
+add_action('widgets_init','followandrew_widget_areas');
+
 
 ?>
--- header.php
+++ header.php
@@ -50,16 +50,13 @@
 				?>
 
 				<hr>
-				<ul class="social-list list-inline py-3 mx-auto">
-					<li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
-					<li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in fa-fw"></i></a></li>
-					<li class="list-inline-item"><a href="#"><i class="fab fa-github-alt fa-fw"></i></a></li>
-					<li class="list-inline-item"><a href="#"><i class="fab fa-stack-overflow fa-fw"></i></a></li>
-					<li class="list-inline-item"><a href="#"><i class="fab fa-codepen fa-fw"></i></a></li>
-				</ul>
+				
+				</div>
 
-			</div>
 		</nav>
+		    <?php
+			    dynamic_sidebar('sidebar-1')
+			?>
     </header>
 	<div class="main-wrapper">
 	    <header class="page-title theme-bg-light text-center gradient py-5">
